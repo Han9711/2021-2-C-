9a4d3e46a5967078630971336d97fb7c20c957d8
Han9711 <hcw9711@naver.com>
2021-11-14 21:02:41 +0900

    첫주차대면수업

--- 2021학년 2학기 C++/소스.cpp
+++ 2021학년 2학기 C++/소스.cpp
@@ -1,9 +1,7 @@
 /*----------------------------------------------------------------------------------------
-  2021 2학기 C++														        7주 2일 - 2교시
+  2021 2학기 C++				               						  수34목23		   11주 1일 
   
-  사용자가 새로운 자료형 You를 만든다.
-
-  string과 같은 기능을 갖는 String을 만들며 class 설계에 필요한 것들을 학습
+  class 간의 관계 - 상속(inheritance)
   ----------------------------------------------------------------------------------------*/
 
 #include <iostream>
@@ -11,62 +9,43 @@
 #include "save.h"
 using namespace std;
 
-// [문제] 다음 main()이 문제없이 실행되도록 class String을 코딩하라.
-// main() 이 수정되면 안된다.
+// 부모 - 자식 클래스 상속을 이용하여 표현해 본다.
+// 메모리 관계는? 멤버함수 이용은?
 
-class String {
-	size_t num{};	// 저장한 글자 수
-	char* p{};	// 글자가 있는 메모리
+class Animal {
+	string name;
+	int age;
 
 public:
-	String (const char* str) : num{ strlen(str) } {
-		// 1. 글자 수를 센다
-		num = strlen(str);
-		// 2. 센 수 만큼 글자를 저장할 공간을 요청한다.
-		p = new char[num];
-		// 3. 그 공간에 p가 가리키는 글자를 저장한다.
-		memcpy(p, str, num);
+	Animal(string s, int n) : name{s}, age{n} {}
+};
 
-		cout << "생성자(const char*) - 갯수:" << num << ", 주소:" << (void*)p << endl;
-	}
+class Dog : public Animal {
+
+public:
+	Dog(string s, int n) : Animal(s, n) {}
 	
-	~String() {
-		cout << "소멸자 -" << num << ", 주소:" << (void*)p << endl;
-		delete[] p;
-	}
+	friend ostream& operator<<(ostream& os, const Dog& dog);
 
-	String& operator=(const String& other) {
-		if (this == &other) {
-			return *this;
-		}
-
-		delete[] p;
-		num = other.num;
-		p = new char[num];
-
-		memcpy(p, other.p, num);
-
-		return *this;
-	}
-
-	void show() {
-		for (int i = 0; i < num; ++i)
-			cout << p[i];
-		cout << endl;
+	string getName() const {
+		return name;
 	}
 };
+
+ostream& operator<<(ostream& os, const Dog& dog)
+{
+	os.dog.getName() << ", " << dog.getAge() << endl;
+}
 
 //---------
 int main()   
 //---------
 {
-	String s1{ "안녕하세요" };
-	String s2{ "C++ 정말 좋은 프로그래밍 언어이다." };
+	Dog dog{ "코코", 2 };
 
-	s2 = s1;
-
-	s1.show();
-	s2.show();
+	cout << dog << endl;
 
 	/*save("소스.cpp");*/  
 }
+
+
