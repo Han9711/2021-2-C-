8c290de8f20cd3cbd24526720affcb4660ad71e2
Han9711 <hcw9711@naver.com>
2021-11-17 13:16:56 +0900

    12주1일차

--- 2021학년 2학기 C++/소스.cpp
+++ 2021학년 2학기 C++/소스.cpp
@@ -1,101 +1,54 @@
 /*----------------------------------------------------------------------------------------
-  2021 2학기 C++				               						         		   9주 2일 
-  
-  표준 string과 유사한 기능을 하는
-      string을 만들며 class 설계에 필요한 것들을 학습
+  2021 2학기 C++				               						  수34목23		   12주 1일
 
-  책 - 10장 클래스 생성자와 소멸자
-       11장 친구 관계와 연산자 오버로딩
-	   13.01 문자열 클래스
+  class 간의 관계 - 상속(inheritance)
+
+  1. 부모/ 자식 - 메모리는 어떻게 구성되나?
   ----------------------------------------------------------------------------------------*/
 
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include "save.h"
 using namespace std;
 
-class String {
-	size_t num{};
-	char* p;
+class Animal {
+protected:
+	string name;
 
 public:
-	String() {
-		cout << "디폴트 생성자() - 갯수:" << num << ", 주소:" << (void*)p << endl;
-	}
+	Animal() {}
+	Animal(string s) : name {s} {}
+	~Animal() {}
 
-	String(const char* str) : num{ strlen(str) } {
-		p = new char[num];
-		memcpy(p, str, num);
-
-		cout << "생성자(const char*) - 갯수: " << num << ", 주소:" << (void*)p << endl;
-	}
-
-	~String() {
-		delete[] p;
-
-		cout << "dtor - Mem(int), 갯수:" << num << ", 주소:" << p << endl;
-
-	}
-
-	String(const String& other) : num{ other.num }, p{ new char[num] } {
-		memcpy(p, other.p, num);
-
-		cout << "복사생성자(const String&) - 갯수: " << num << ", 주소:" << (void*)p << endl;
-	}
-
- 	String& operator=(const String& other) {
-		if (this == &other) {
-			return *this;
-		}
-
-		delete[] p;
-		num = other.num;
-		p = new char[num];
-
-		memcpy(p, other.p, num);
-
-		cout << "할당연산자 - 갯수:" << num << ", 주소:" << (void*)p << endl;
-	}
-
-	// 연산자 오버로딩
-
-	String operator+(const String& rhs) const {
-		String temp;
-		temp.num = num + rhs.num;
-		temp.p = new char[temp.num];
-
-		memcpy(temp.p, p, num);
-		memcpy(temp.p + num, rhs.p, rhs.num);
-
-		return temp;
-	}
-
-
-	size_t size() const {
-		return num;
-	}
-
-	friend ostream& operator<<(ostream&, const String&);
+	void move() { cout << "Animal 움직인다" << endl; }
 };
 
-ostream& operator<<(ostream& os, const String& s)
-{
-	for (int i = 0; i < s.num; ++i)
-		os << s.p[i];
-	return os;
-}
+class Dog : public Animal {
 
-// 자원을 관리하는 클래스
-// 자기가 관리하는 자원을 이동시킬 수도 있다.
-// 이동은 복사보다 효율적이 동작이 될 수 있다.
+public:
+
+	Dog() {}
+	Dog(string s) : Animal{ s } {  }
+	~Dog() {
+		cout << "소멸자생성" << endl;
+	}
+
+	void move() { cout << this->name << "뛴다" << endl; }
+
+	// this가 숨어서 멤버 변수를 호출한다.
+};
 
 //---------
-int main()   
+int main()
 //---------
 {
-	printf("Hello World\n");
+	Dog a{ "코코" };		// 코코가 뛴다
+	Dog b{ "보리" };		// 보리가 뛴다
 
-	/*save("소스.cpp");*/  
+	a.move();
+	b.move();
+	
+	/*save("소스.cpp");*/
 }
+
 
